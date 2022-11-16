99be69faa9486582f18b10f77db8cab197624032
Brandon Roman <116917326+flipsun@users.noreply.github.com>
2022-11-16 15:21:22 -0800

    Adding Basic JavaScript Projects Submission

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("Hello, world!");
+
+var sent1 = "This is the beginning of the string"; //Comment for sent1
+
+var sent2 = " and this is the end of the string"; //Comment for sent2
+
+document.write(sent1+sent2); //Comment for Concatenating variables
+
+document.write(6+6); //Comment for expression
