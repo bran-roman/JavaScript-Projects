795d0e39d17c312c7e8e948ec58b851c95d01c1c
Brandon Roman <116917326+flipsun@users.noreply.github.com>
2022-11-17 14:08:26 -0800

    Adding Project4_Dictionaries Submission

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,23 @@
+function my_Dictionary(){
+    var Animal = {
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age:5,
+        Sound:"Bark!"
+    };
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+}
+
+function my_Dictionary2(){
+    var Animal = {
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age:5,
+        Sound:"Bark!"
+    };
+    delete Animal.Sound;
+    document.getElementById("Dictionary2").innerHTML = Animal.Sound;
+}
+
