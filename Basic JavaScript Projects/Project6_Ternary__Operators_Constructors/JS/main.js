a052e66ec934873d2e75f7a8773eeb5c53bb8f47
Brandon Roman <116917326+flipsun@users.noreply.github.com>
2022-11-18 12:15:44 -0800

    Edited Project6 index and js

--- Basic JavaScript Projects/Project6_Ternary__Operators_Constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_Ternary__Operators_Constructors/JS/main.js
@@ -1,25 +1,28 @@
-function Vote_Function() {
+function Vote_Function() { /*Voting Function*/
     var Age, Can_Vote;
     Age = document.getElementById("Age").value;
     Can_Vote = (Age < 18) ? "You can not vote":"You can vote";
     document.getElementById("Ride").innerHTML = Can_Vote;
 }
 
-function Vehicle(Make, Model, Year, Color) {
+function Vehicle(Make, Model, Year, Color) { /*Vehicle Function*/
     this.Vehicle_Make = Make;
     this.Vehicle_Model = Model;
     this.Vehicle_Year = Year;
     this.Vehicle_Color = Color;
 }
 
+/*Name and Vehicle variables for function "myFunction()" */
 var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
 var Emily = new Vehicle("Jeeo", "Trail Hawk", 2019, "White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 
+/*Displays name of variable + what car they drive when user clicks button*/
 function myFunction() {
     document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
 }
 
+/*Name Function*/
 function Person(first, last, age, eye) {
     this.firstName = first;
     this.lastName = last;
@@ -27,12 +30,15 @@ function Person(first, last, age, eye) {
     this.eyeColor = eye;
 }
 
+/*Constructor for function "myPerson"*/
 const myBrother = new Person("Brandon", "Roman", 26, "brown"); /* Creates a Person Object */
 
+/*Displays person's age when user clicks on button*/
 function myPerson() {
     document.getElementById("bro").innerHTML = "My brother is " + myBrother.age + ".";
 }
 
+/*Displays the reuslt of "10" because the Plus_one() function was the nested function*/
 function nest_Function() {
     document.getElementById("nesting").innerHTML = nest();
     function nest() {
